7759a029ab0c249d92460d4adb7afdae716cf030
jiyoung park <asa855@nate.com>
2024-04-06 16:53:19 +0900

    [feat] 이미지, 폰트 추가

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ weight: ['400'], subsets: ['greek'] });
+export const lusitana = Lusitana({ weight: ['700'], subsets: ['latin'] });

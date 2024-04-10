26f9d8628600fa205372fefd8e0b006a88f3a71f
jiyoung park <asa855@nate.com>
2024-04-10 23:36:33 +0900

    [feat] adding search and pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -120,6 +120,7 @@ export async function fetchFilteredInvoices(
       ORDER BY invoices.date DESC
       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
     `;
+    console.log(invoices.rows);
 
     return invoices.rows;
   } catch (error) {
